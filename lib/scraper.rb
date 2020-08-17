6b51771aad402da00d164ad751a3cd865e7adb31
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-17 14:43:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,12 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    student_info = doc.css("".student-card a")
+    student_info = doc.css(".student-card a")
+    student_info.collect do |element|
+      {:name => element.css(".student-name").text, 
+      :location => element.css("student-location").text, 
+      :profile_url => element.attr("href")
+    }
 
   end
 
