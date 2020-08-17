ae314749d725a40741ab7977de87033149bd981d
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-17 15:09:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,8 +30,7 @@ class Scraper
           student_profile[:github] = element.attr("href")
         elsif element.attr("href").include?("blog")
           student_profile[:blog] = element.attr("href")
-        elsif element.attr("href").include?("profile_quote")
-          student_profile[:profile_quote] = element.attr("href")
+        end  
 
 
 
