e4d4b54984936878aa9b03b6099f8ad87ccf2eb1
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-17 14:46:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)  #is a class method that scrapes the student index page ('./fixtures/student-site/index.html') and a returns an array of hashes in which each hash represents one student
-    html = open(index_url)
+    html = open(index_url)  #Nokogiri and Open-URI to access that page.
     doc = Nokogiri::HTML(html)
     student_info = doc.css(".student-card a")
     student_info.collect do |element|
