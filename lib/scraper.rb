a289184d5997df4918f7f73a53a4ea3e083c6809
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-17 15:20:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,8 +35,9 @@ class Scraper
       student_profile[:profile_quote] = doc.css(".vitals-container .vitals-text-containter .profile-quote").text
       student_profile[:bio] = doc.css(".bio-block.detils-block .bio-content.content-holder .description-holder p").text
 
-
-
+  student_profile
   end
+
+
 
 end
