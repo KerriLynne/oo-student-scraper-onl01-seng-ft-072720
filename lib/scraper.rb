f162697638bacd2dbef12563a5a9ac74c1b6f9dc
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-17 14:12:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
